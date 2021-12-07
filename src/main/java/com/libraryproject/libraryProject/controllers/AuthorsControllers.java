ef6910334464b91edc797e3e0e98032ae3a3a10e
Vija-M <vija.riga@gmailcom>
2021-12-07 11:59:59 +0200

    Authors Controllers

--- src/main/java/com/libraryproject/libraryProject/controllers/AuthorsControllers.java
+++ src/main/java/com/libraryproject/libraryProject/controllers/AuthorsControllers.java
@@ -1,4 +1,24 @@
 package com.libraryproject.libraryProject.controllers;
 
+
+import com.libraryproject.libraryProject.domain.Authors;
+import com.libraryproject.libraryProject.repositories.AuthorsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+/*@Controller
+@RequestMapping(path = "/books")*/
 public class AuthorsControllers {
+
+    @Autowired
+    private AuthorsRepository authorRepository;
+
+    @GetMapping(value = "/allAuthors")
+    public @ResponseBody
+    Iterable<Authors> getAll() {
+        return authorRepository.findAll();
+    }
 }
--- src/main/java/com/libraryproject/libraryProject/repositories/AuthorsRepository.java
+++ src/main/java/com/libraryproject/libraryProject/repositories/AuthorsRepository.java
@@ -1,4 +1,9 @@
 package com.libraryproject.libraryProject.repositories;
 
-public interface AuthorsRepository {
+import com.libraryproject.libraryProject.domain.Authors;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorsRepository extends JpaRepository<Authors, Integer> {
+
 }
+
