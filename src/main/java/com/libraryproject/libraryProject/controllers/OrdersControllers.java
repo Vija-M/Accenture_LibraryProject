c0d0f4af444d82becd13a28a54e77239c119f35f
Vadims Burovs <burovs.vadim@gmail.com>
2021-12-07 11:35:59 +0200

    Added LibraryController for login page.

--- src/main/java/com/libraryproject/libraryProject/controllers/OrdersControllers.java
+++ src/main/java/com/libraryproject/libraryProject/controllers/OrdersControllers.java
@@ -1,14 +1,6 @@
 package com.libraryproject.libraryProject.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.*;
 
-
-@Controller
-@RequestMapping()
 public class OrdersControllers {
 
 
