8d2179c44d98dcf1c599f13663d72a1ae97965f5
Vija-M <vija.riga@gmailcom>
2021-12-07 13:55:59 +0200

    Authors Controllers

--- src/main/java/com/libraryproject/librarysystem/controllers/AuthorsControllers.java
+++ src/main/java/com/libraryproject/librarysystem/controllers/AuthorsControllers.java
@@ -1,4 +1,23 @@
 package com.libraryproject.librarysystem.controllers;
 
+import com.libraryproject.librarysystem.domain.Authors;
+import com.libraryproject.librarysystem.repositories.AuthorsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
 public class AuthorsControllers {
+
+    @Autowired
+    private AuthorsRepository authorRepository;
+
+    @GetMapping(value = "/allAuthors")
+    public @ResponseBody
+    Iterable<Authors> getAll() {
+        return authorRepository.findAll();
+    }
 }
