641ffbc75ef8f36e0db49c43be492a0fd0759d8d
Ignas <81799445+igneqas@users.noreply.github.com>
2021-12-07 13:21:33 +0200

    Added new project

---
+++ LibrarySystemProject/src/main/java/com/libraryproject/librarysystem/controllers/BooksControllers.java
@@ -0,0 +1,29 @@
+package com.libraryproject.librarysystem.controllers;
+
+import com.libraryproject.librarysystem.domain.Availability;
+import com.libraryproject.librarysystem.domain.Books;
+import com.libraryproject.librarysystem.repositories.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping
+public class BooksControllers {
+
+    @Autowired
+    private BooksRepository booksRepository;
+
+    @GetMapping(value = "/allBooks")
+    public @ResponseBody
+    Iterable<Books> getAll () {
+        return booksRepository.findAll();
+    }
+
+    /*@PostMapping(value = "/addBook")
+    public @ResponseBody String addBook(@RequestParam String title) {
+        Books book = new Books(title, Availability.AVAILABLE);
+        booksRepository.save(book);
+        return "Success";
+    }*/
+}
